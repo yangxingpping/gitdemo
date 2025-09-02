9121bbe24fbf6eaec7986361330e1870bf0df024
ggyy <alqaz@outlook.com>
2025-09-02 16:22:54 +0800

    主分支添加实现

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 
+#include <map>
 #include <iostream>
+using std::map;
 using namespace std;
 
 int main() {
     cout << "Hello, World!" << endl;
+    map<int , int> m;
+    m[1] = 2;
+    m[2] = 3;
+    m[3] = 4;
     return 0;
 }
